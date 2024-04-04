902e6ab7522e9e2e9bcda940e64e6d0e5aefa34d
otaku-soft <flamehiro@gmail.com>
2024-04-04 15:22:27 -0600

    Login/register/reset password

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,20 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            server: {
+                host: '0.0.0.0',
+                hmr: {
+                    host: 'localhost'
+                },
+                watch: {
+                    // https://vitejs.dev/config/server-options.html#server-watch
+                    usePolling: true
+                }
+            },
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
