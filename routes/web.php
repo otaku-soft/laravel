0a11e129f40af92e281471ed365a3a6c71214998
otaku-soft <flamehiro@gmail.com>
2024-04-09 02:02:52 -0600

    index page

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/forum', function () {
+    return view('forum/index');
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
