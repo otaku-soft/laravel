47b5e680bbb2a4bde90a474ad47d948f4b8aa884
otaku-soft <flamehiro@gmail.com>
2024-05-14 16:43:26 -0600

    add soft delete to models

--- app/Models/forums_categories.php
+++ app/Models/forums_categories.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class forums_categories extends Model
 {
     use HasFactory;
-
+    use SoftDeletes;
     public function topics(): HasMany
     {
         return $this->hasMany(forums_topics::class,"category_id");
